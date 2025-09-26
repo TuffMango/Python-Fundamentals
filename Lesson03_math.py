f70fa44eff4e55653e9fb187d8cbbbef77797714
TuffMango <therealgeorege@gmail.com>
2025-09-26 18:00:10 +0000

    Math stuff

---
+++ Lesson03_math.py
@@ -0,0 +1,34 @@
+#Key concepts: Math operators: +, -, *, /, //, %, **
+add = 7 + 2
+print("sum" , add)
+
+subtract = 2 - 5
+print("difference:"  , subtract)
+mutiply = 6 * 3
+print("product:" , mutiply)
+
+floatdivide = 7 / 2
+print("Float division:" , floatdivide)
+Integerdivide = 7 // 2
+print("Integer Division:" , Integerdivide) #floor Division
+
+#Modulus
+Modulus = 7 % 2
+print("Modulus:" , Modulus) #remainders of dividing
+
+exponent = 7 ** 2
+print("Exponent: " , exponent)
+
+print(2 + 2 == 10 - 6)
+result1 = (2+3) * 2 **4
+print("Answer:" , result1)
+
+result2  = 20 / 5 * 2
+print("Float Answer: \t" , result2)
+
+
+result4 = 10 - 2 + 3
+print("result 4: \t" , result4)
+
+Result5 = 5 +2 ** 3 * (4 - 1)
+print("answer five: \t" , Result5)
