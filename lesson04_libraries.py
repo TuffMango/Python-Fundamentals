3c6c40121e3facd27e54270b1172ff7c4f288d41
TuffMango <therealgeorege@gmail.com>
2025-10-01 17:26:35 +0000

    library stuff

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -24,3 +24,24 @@ step_one = seed / 321
 step_2 = step_one % 30
 step_3 = step_2**2 / 4
 print("Random seed thing:", math.floor(step_3))
+
+import random #makes random stuff 
+
+#methods:
+print("random integer: " , random.randint(1, 100))
+print(random.random()) #random float
+print(random.choice(["eggs", "cheese", "chicken"])) 
+mylist=["nufersinou", "uferioapnu", "gaieneriu"]
+print(random.choice(mylist))
+random.shuffle(mylist)
+print(mylist)
+
+
+
+#challange
+radius = 7
+circle_area = math.pi* radius ** 2
+print(math.floor(circle_area))
+
+print(random.randint(1, 6))
+
