c674f153432a060c542c91fc52cada612f8a55db
TuffMango <therealgeorege@gmail.com>
2025-10-09 17:57:34 +0000

    Strings

--- Lesson06_strings.py
+++ Lesson06_strings.py
@@ -24,3 +24,35 @@ phrase = "spOngEbOb"
 print("uppercase:", phrase.upper())
 #opposite for lowercase
 print("Capitilized:", phrase.capitalize())
+
+#Find and replace text
+sentence = "I am you."
+new_sentence = sentence.replace("Im",  "Youre")
+other_replacement = sentence.replace("am", "are")
+last_replacement = sentence.replace("you.", "me.")
+print(sentence)
+print(other_replacement)
+
+#FORMATTED STRINGS
+
+name = "Ada"
+age = 28
+city = "London"
+print(f"Hello my name is {name}. I am {age} years  old and live in {city}")
+
+print(f"Next year, ill be {age+1}")
+
+Favorite_quote = input("Enter your favorite quote.")
+length = (Favorite_quote)
+sentence = ("Your quote is " + Favorite_quote + " and it is " + str(len(length)) + " letters long")
+print(sentence)
+
+First = input("First name:")
+last = input("Last name:")
+Info = last + ", " + First
+print(Info)
+
+word = "Yotei"
+print(word.upper())
+print(word [::-1])
+print(word.lower())
